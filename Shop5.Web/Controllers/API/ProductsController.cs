5cb546a6eac84dc1942152239bb5e5740c9bb570
ERIKA BENITEZ <erikaitm546@gmail.com>
2019-02-15 19:00:07 -0500

    create Api producst controller

---
+++ Shop5.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop5.Web.Controllers.API
+{
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using System;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+        private readonly ILogger<ProductsController> logger;
+
+        public ProductsController(IProductRepository productRepository, ILogger<ProductsController> logger)
+        {
+            this.productRepository = productRepository;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            try
+            {
+                return this.Ok(this.productRepository.GetAll());
+            }
+            catch (Exception ex)
+            {
+                var message = $"Error: {ex}";
+                this.logger.LogError(message);
+                return this.BadRequest(message);
+            }
+        }
+    }
+
+}
